bbeac0cc93f40ec96d597a5828ded01cdae27e62
Marco <mdalai2394@gmail.com>
2019-06-05 10:05:16 +0200

    Refactoring

---
+++ DatasetDistillation/distillation/base_distillation_trainer.py
@@ -0,0 +1,50 @@
+
+
+class BaseDistillationTrainer(object):
+
+    def __init__(self, model, optimization_iterations, data_size, weights_init_fn, learning_rate, weights_batch_size,
+                 loss_fn, alpha, device):
+        self.model = model
+        self.T = optimization_iterations
+        self.device = device
+        self.weights_init_fn = weights_init_fn
+        self.eta = learning_rate
+        self.weights_batch_size = weights_batch_size
+        self.data_size = data_size
+        self.loss_fn = loss_fn
+        self.alpha = alpha
+        self.__distilled_data = None
+        self.__distilled_learning_rate = None
+        self.__distilled_targets = None
+
+    @property
+    def distilled_data(self):
+        if self.__distilled_data is None:
+            raise ValueError('No distilled data are available.')
+        return self.__distilled_data
+
+    @property
+    def distilled_learning_rate(self):
+        if self.__distilled_learning_rate is None:
+            raise ValueError('No distilled learning rate is available.')
+        return self.__distilled_learning_rate
+
+    @property
+    def distilled_targets(self):
+        if self.__distilled_targets is None:
+            raise ValueError('No distilled targets are available.')
+        return self.__distilled_targets
+
+    def __get_next_batch(self, it, loader):
+        try:
+            b = next(it)
+        except StopIteration:
+            it = iter(loader)
+            b = next(it)
+        return b, it
+
+    def distill(self, *args):
+        raise NotImplementedError()
+
+    def train(self, *args):
+        raise NotImplementedError()
--- DatasetDistillation/distillation/classification_distillation_trainer.py
+++ DatasetDistillation/distillation/classification_distillation_trainer.py
@@ -1,34 +1,19 @@
 import torch
 
 from utils import MetaModelUtils
+from .base_distillation_trainer import BaseDistillationTrainer
 
 
-class ClassificationDistillationTrainer(object):
+class ClassificationDistillationTrainer(BaseDistillationTrainer):
 
     def __init__(self, model, optimization_iterations, data_size, weights_init_fn, learning_rate, weights_batch_size,
                  loss_fn, alpha, device):
-        self.model = model
-        self.T = optimization_iterations
-        self.device = device
-        self.weights_init_fn = weights_init_fn
-        self.eta = learning_rate
-        self.weights_batch_size = weights_batch_size
-        self.data_size = data_size
-        self.loss_fn = loss_fn
-        self.alpha = alpha
-        self.distilled_labels = None
-        self.distilled_data = None
-        self.distilled_learning_rate = None
+        super(ClassificationDistillationTrainer, self).__init__(model, optimization_iterations, data_size,
+                                                                weights_init_fn, learning_rate, weights_batch_size,
+                                                                loss_fn, alpha, device)
+        self.__distilled_labels = None
 
-    def __get_next_batch(self, it, loader):
-        try:
-            b = next(it)
-        except StopIteration:
-            it = iter(loader)
-            b = next(it)
-        return b, it
-
-    def distill_(self, training_data_loader, n_labels, exemples_per_label):
+    def distill(self, training_data_loader, n_labels, exemples_per_label):
         distilled_labels = torch.arange(n_labels).repeat(exemples_per_label).reshape(exemples_per_label, 1)\
             .transpose(1, 0).reshape(-1)
         distilled_data_size = torch.Size([distilled_labels.size(0)]) + torch.Size(self.data_size)
@@ -53,36 +38,37 @@ class ClassificationDistillationTrainer(object):
                 # Weights batch init
                 self.model.apply(self.weights_init_fn)
 
-                self.model.zero_grad()
+                with torch.enable_grad():
+                    self.model.zero_grad()
 
-                out = self.model(distilled_data)
-                loss = self.loss_fn(out, distilled_labels)
+                    out = self.model(distilled_data)
+                    loss = self.loss_fn(out, distilled_labels)
 
-                # Backward on loss
-                # Flag retain_graph=True allows to maintain the computational graph
-                # Flag create_graph=True allows to create the graph on the computed derivatives
-                loss.backward(retain_graph=True, create_graph=True)
+                    # Backward on loss
+                    # Flag retain_graph=True allows to maintain the computational graph
+                    # Flag create_graph=True allows to create the graph on the computed derivatives
+                    loss.backward(retain_graph=True, create_graph=True)
 
-                # Get flat weights and flat grad to perform the update on the weights
-                flat_weights = MetaModelUtils.get_flat_params(self.model)
-                flat_grads = MetaModelUtils.get_flat_grads(self.model)
-                flat_grads = self.eta.item() * flat_grads
-                flat_weights = flat_weights - flat_grads
-                MetaModelUtils.set_flat_params(self.model, flat_weights)
+                    # Get flat weights and flat grad to perform the update on the weights
+                    flat_weights = MetaModelUtils.get_flat_params(self.model)
+                    flat_grads = MetaModelUtils.get_flat_grads(self.model)
+                    flat_grads = self.eta.item() * flat_grads
+                    flat_weights = flat_weights - flat_grads
+                    MetaModelUtils.set_flat_params(self.model, flat_weights)
 
-                # Compute the loss on the training_data
-                self.model.zero_grad()
-                out = self.model(data)
-                final_loss = self.loss_fn(out, labels)
+                    # Compute the loss on the training_data
+                    self.model.zero_grad()
+                    out = self.model(data)
+                    final_loss = self.loss_fn(out, labels)
 
-                # Performing now the backward allows to obtain the gradients of the distilled data
-                # and distilled learning rate. This time is not necessary to set retain_graph=True and
-                # create_graph=True
-                final_loss.backward()
+                    # Performing now the backward allows to obtain the gradients of the distilled data
+                    # and distilled learning rate. This time is not necessary to set retain_graph=True and
+                    # create_graph=True
+                    final_loss.backward()
 
-                # Save gradients of distilled data and distilled learning rate
-                distilled_data_grads.append(distilled_data.grad)
-                eta_grads.append(eta.grad)
+                    # Save gradients of distilled data and distilled learning rate
+                    distilled_data_grads.append(distilled_data.grad)
+                    eta_grads.append(eta.grad)
 
             # Set both distilled_data and eta to requires_grad=False
             distilled_data.requires_grad_(False)
@@ -91,8 +77,9 @@ class ClassificationDistillationTrainer(object):
             eta -= self.alpha * torch.tensor(eta_grads, device=self.device).sum()
 
         self.model.eval()
-        self.distilled_data = distilled_data.detach()
-        self.distilled_labels = distilled_labels
-        self.distilled_learning_rate = eta.detach()
+        self.__distilled_data = distilled_data.detach()
+        self.__distilled_labels = distilled_labels
+        self.__distilled_learning_rate = eta.detach()
 
-        return self.distilled_data, self.distilled_labels, self.distilled_learning_rate
+    def train(self, *args):
+        pass
